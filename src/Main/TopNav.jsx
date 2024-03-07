7f4d983f1413264f691f98f4c4664ce63597c110
RahulJaincorpseed <rahul.jain@corpseed.com>
2024-03-07 14:13:11 +0530

    add login and sign up pages and some ui changes

--- src/Main/TopNav.jsx
+++ src/Main/TopNav.jsx
@@ -3,7 +3,29 @@ import "./TopNav.scss"
 
 const TopNav = () => {
   return (
-    <div>TopNav</div>
+    <div className="top-navbar">
+    <div className="top-search-box">
+      {/* <i className="fa-solid search-icon fa-magnifying-glass"></i>
+      <input className="search-box" type="search" placeholder="Serach..." /> */}
+    </div>
+    {/* <div className="user-profile">
+      <button className="btn btn-primary" onClick={logoutUser}>Logout</button>
+      <div className="profile-image">
+        <img
+          src={`https://images.pexels.com/photos/17739178/pexels-photo-17739178.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1`}
+        />
+      </div>
+      <div className="profile-info">
+        <h4>Rahul jain</h4>
+        <h5>Corpseed</h5>
+      </div>
+    </div> */}
+    <div>
+      <div className="notes-box">
+    
+      </div>
+    </div>
+  </div>
   )
 };
 
