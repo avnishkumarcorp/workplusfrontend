9b4ebc4951a18a488cccd420ec69ceb78e21723f
RahulJaincorpseed <rahul.jain@corpseed.com>
2024-03-08 10:59:41 +0530

    add bar chart

--- src/data/fakeData.jsx
+++ src/data/fakeData.jsx
@@ -1,44 +1,134 @@
 export const fakeData = [
     {
-      name: 'Page A',
+      name: '1',
       uv: 4000,
       pv: 2400,
       amt: 2400,
     },
     {
-      name: 'Page B',
+      name: '2',
       uv: 3000,
       pv: 1398,
       amt: 2210,
     },
     {
-      name: 'Page C',
+      name: '3',
       uv: 2000,
       pv: 9800,
       amt: 2290,
     },
     {
-      name: 'Page D',
+      name: '4',
       uv: 2780,
       pv: 3908,
       amt: 2000,
     },
     {
-      name: 'Page E',
+      name: '5',
       uv: 1890,
       pv: 4800,
       amt: 2181,
     },
     {
-      name: 'Page F',
+      name: '6',
       uv: 2390,
       pv: 3800,
       amt: 2500,
     },
     {
-      name: 'Page G',
+      name: '7',
       uv: 3490,
       pv: 4300,
       amt: 2100,
     },
+    {
+        name: '8',
+        uv: 3490,
+        pv: 4300,
+        amt: 2100,
+      },
   ];
+
+  export const barData = 
+  [
+    {
+      name: '09:00',
+      uv: 4000,
+      pv: 2400,
+      amt: 2400,
+    },
+    {
+      name: '10:00',
+      uv: 3000,
+      pv: 1398,
+      amt: 2210,
+    },
+    {
+      name: '11:00',
+      uv: 2000,
+      pv: 9800,
+      amt: 2290,
+    },
+    {
+      name: '12:00',
+      uv: 2780,
+      pv: 3908,
+      amt: 2000,
+    },
+    {
+      name: '01:00',
+      uv: 1890,
+      pv: 4800,
+      amt: 2181,
+    },
+    {
+      name: '02:00',
+      uv: 2390,
+      pv: 3800,
+      amt: 2500,
+    },
+    {
+      name: '03:00',
+      uv: 3490,
+      pv: 4300,
+      amt: 2100,
+    },
+    {
+        name: '04:00',
+        uv: 1890,
+        pv: 4800,
+        amt: 2181,
+      },
+      {
+        name: '05:00',
+        uv: 2390,
+        pv: 3800,
+        amt: 2500,
+      },
+      {
+        name: '06:00',
+        uv: 3490,
+        pv: 4300,
+        amt: 2100,
+      },
+      {
+        name: '07:00',
+        uv: 1890,
+        pv: 4800,
+        amt: 2181,
+      },
+      {
+        name: '08:00',
+        uv: 2390,
+        pv: 3800,
+        amt: 2500,
+      },
+      {
+        name: '09:00',
+        uv: 3490,
+        pv: 4300,
+        amt: 2100,
+      },
+  ];
+
+
