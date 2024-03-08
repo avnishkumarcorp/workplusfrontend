cd61f8f70c145f411606608df7d2d6f86e217bad
RahulJaincorpseed <rahul.jain@corpseed.com>
2024-03-08 11:33:02 +0530

    add sidebar button design

--- src/Main/MainOutlet.jsx
+++ src/Main/MainOutlet.jsx
@@ -2,12 +2,13 @@ import React from "react";
 import "./MainOutlet.scss"
 import { Outlet } from "react-router-dom";
 import TopNav from "./TopNav";
+import SideBar from "./SideBar";
 
 const MainOutlet = () => {
   return (
     <div className="main-page">
       <div className="side-nav">
-       <h1>sidebar</h1>
+       <SideBar />
       </div>
       <div className="right-data">
         <TopNav />
--- src/Main/SideBar.jsx
+++ src/Main/SideBar.jsx
@@ -1,11 +1,16 @@
 import React from "react"
 import "./SideBar.scss"
 import { NavLink } from "react-router-dom"
+import SideBarBtn from "../Components/SideBarBtn"
 
 const SideBar = () => {
   return (
     <div className="sidebar-data">
-      <div className="links-data"></div>
+      <h3><i class="fa-regular mr-2 fa-clock"></i>DeskTime</h3>
+      <div className="side-buttons">
+      <SideBarBtn name="My Desktime" icon={<i class="fa-solid fa-desktop"></i>} />
+      <SideBarBtn name="Screen Shot" icon={<i class="fa-regular fa-image"></i>} />
+      </div>
     </div>
   )
 }
