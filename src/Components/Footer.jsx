f6cb7e4f0801565a05fe1f9f31a367a17bb1b825
RahulJaincorpseed <rahul.jain@corpseed.com>
2024-05-02 15:09:58 +0530

    add homepage

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,7 +1,13 @@
-import React from "react";
+import React from "react"
+import "./Navbar.scss"
 
 const Footer = () => {
-  return <div>Footer</div>;
-};
+  return (
+    <div className="navbar">
+      <div className="desktime-logo"><i class="fa-solid mr-1 fa-clock"></i>Desktime 2024 All Right Reserved</div>
+      <div className="text-white">Stay Connect!</div>
+      </div>
+  )
+}
 
-export default Footer;
+export default Footer
