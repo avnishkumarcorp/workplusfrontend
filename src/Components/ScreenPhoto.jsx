cb4fcdc5ea781d5a535c318ee0b26eedd063b6f4
RahulJaincorpseed <rahul.jain@corpseed.com>
2024-05-10 16:36:34 +0530

    add some user data

--- src/Components/ScreenPhoto.jsx
+++ src/Components/ScreenPhoto.jsx
@@ -1,11 +1,30 @@
-import React from "react"
+import React, { useState } from "react"
 import "./ScreenPhoto.scss"
 import { Link } from "react-router-dom"
 
 const ScreenPhoto = ({ index, image, time, date }) => {
+  const [imgClick, setImgClick] = useState(false)
+  const [icon, setIcon] = useState(true)
+
+  const handleChange = () => {
+    setImgClick((prev) => !(prev))
+    setIcon((prev) => !(prev))
+  }
+  
+  const closeImage = () => {
+    setImgClick(false)
+    setIcon(false)
+
+    console.log("i am click ", imgClick, icon);
+  }
+
   return (
     <div key={index}>
-      <div className="image-box">
+      <div
+        onClick={handleChange}
+        className={`image-box ${imgClick ? "image-box-click" : ""}`}
+      >
+        {icon ? <i onClick={closeImage}  class="fa-solid  my-circle-plus fa-circle-plus"></i> : ""}
         <img src={image} />
       </div>
       <div className="sm-date">
@@ -20,7 +39,9 @@ const ScreenPhoto = ({ index, image, time, date }) => {
           </p>
         </div>
         <div>
-          <Link className="cm-btn-one" to={image}>Download</Link>
+          <Link className="cm-btn-one" to={image}>
+            Download
+          </Link>
         </div>
       </div>
     </div>
